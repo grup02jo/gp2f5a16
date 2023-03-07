07595a6a9b72767ccd6774f714ae148b2fb49551
vagrant <vagrant@gp2f5a16>
2023-03-07 16:37:27 +0000

    2.2

--- app/examen.php
+++ app/examen.php
@@ -60,30 +60,28 @@
       <option value="ASIX">ASIX</option>
       <option value="DAM">DAM</option>
       <option value="DAW">DAW</option>
-    </select>    
-/* 1 */
-
+    </select>
+	<br><br>  1.
       <p>Quin és el port típic de DHCP?</p>
       <input type="radio" name="pregunta1" value="a">a. 80<br>
       <input type="radio" name="pregunta1" value="b">b. 67<br>
       <input type="radio" name="pregunta1" value="c">c. 53<br>
       <input type="radio" name="pregunta1" value="d">d. 68<br>
 
-/* 2 */   
- 
+	  <br> 2.
       <p>Quin és el port típic de DNS?</p>
       <input type="radio" name="pregunta2" value="a">a. 80<br>
       <input type="radio" name="pregunta2" value="b">b. 53<br>
       <input type="radio" name="pregunta2" value="c">c. 22<br>
       <input type="radio" name="pregunta2" value="d">d. 25<br>
-    
-/* 3 */
+
+	  <br> 3.
 
       <p>Utilitza UDP o TCP?</p>
       <input type="radio" name="pregunta3" value="a">a. UDP<br>
       <input type="radio" name="pregunta3" value="b">b. TCP<br>
-     
-/* 4 */
+
+	  <br> 4.
 
       <p>Quin és el port típic de HTTP?</p>
       <input type="radio" name="pregunta4" value="a">a. 55<br>
@@ -96,34 +94,33 @@
 
 
 
-     
-/* 5  */
+
+	  <br> 5.
 
        <p>Quin és el port típic del SSH?</p>
       <input type="radio" name="pregunta5" value="a">a. 22<br>
       <input type="radio" name="pregunta5" value="b">b. 47<br>
       <input type="radio" name="pregunta5" value="c">c. 53<br>
       <input type="radio" name="pregunta5" value="d">d. 25<br>
-      
-      
 
-/* 6 */
-   
+
+
+	  <br> 6.
+
    <p>Que es el FTP?</p>
       <input type="radio" name="pregunta6" value="a">a. Protocol de transferencia de arxius<br>
       <input type="radio" name="pregunta6" value="b">b. Secure Shell<br>
       <input type="radio" name="pregunta6" value="c">c. File Transfer Packs <br>
       <input type="radio" name="pregunta6" value="d">d. Protocol de Datagrama de usuari<br>
-      
 
-/* 7 */
+
+	  <br> 7.
 
    <p>Que significa SMTP</p>
       <input type="radio" name="pregunta7" value="a">a . Protocol ocult de transferencia de correus<br>
       <input type="radio" name="pregunta7" value="b">b. Protocol de transferencia de fitxers<br>
       <input type="radio" name="pregunta7" value="c">c. Sistema de muntatge intern fitxers<br>
       <input type="radio" name="pregunta7" value="d">d. Protocol simple de transferencia de    correu<br>
-    
 
 
 
@@ -132,79 +129,80 @@
 
 
 
-/* 8 */
+
+	  <br> 8.
 
    <p>En quin àmbit s’utilitza POP3 ?</p>
       <input type="radio" name="pregunta8" value="a">a. Correu electronic<br>
       <input type="radio" name="pregunta8" value="b">b. Navegadors Internet<br>
       <input type="radio" name="pregunta8" value="c">c. Videojocs<br>
       <input type="radio" name="pregunta8" value="d">d. Fitxers<br>
-     
-/* 9 */
+
+	  <br> 9.
 
    <p>A quin protocol pertany TELNET ?</p>
       <input type="radio" name="pregunta9" value="a">a. SMTP<br>
       <input type="radio" name="pregunta9" value="b">b. TCP<br>
       <input type="radio" name="pregunta9" value="c">c. UDP<br>
       <input type="radio" name="pregunta9" value="d">d. HTTP<br>
-      
 
-/* 10 */
+
+	  <br> 10.
 
    <p>Que signifiquen les sigles LPD?</p>
       <input type="radio" name="pregunta10" value="a">a. Le Protocol Div <br>
       <input type="radio" name="pregunta10" value="b">b.Less Protocol Down<br>
       <input type="radio" name="pregunta10" value="c">c. Los Angeles Police Department<br>
       <input type="radio" name="pregunta10" value="d">d. Line Printer Daemon<br>
-     
 
-/* 11 */
+
+	  <br> 11.
 
    <p>Que signifiquen les sigles LPDA?</p>
       <input type="radio" name="pregunta11" value="a">a. Protocol lleuger de acces a directoris<br>
       <input type="radio" name="pregunta11" value="b">b. Protocol de datagrama universal<br>
       <input type="radio" name="pregunta11" value="c">c. Acces Directe a fitxers<br>
       <input type="radio" name="pregunta11" value="d">d. Lost directory down access<br>
-  
 
-/* 12 */
+
+	  <br> 12.
 
    <p>Que es el protocol Samba?</p>
       <input type="radio" name="pregunta12" value="a">a. Es un protocol de ball<br>
       <input type="radio" name="pregunta12" value="b">b. Transferencia de missatges buits<br>
       <input type="radio" name="pregunta12" value="c">c. Server Message Block <br>
       <input type="radio" name="pregunta12" value="d">d.Ninguna es correcta<br>
-     
-/* 13 */
+
+	  <br> 13.
 
    <p>Quin port utilitza el  NFS?</p>
       <input type="radio" name="pregunta13" value="a">a. 92<br>
       <input type="radio" name="pregunta13" value="b">b. 1034l<br>
       <input type="radio" name="pregunta13" value="c">c. 23 <br>
       <input type="radio" name="pregunta13" value="d">d. 2049<br>
-      <input type="submit" value="Envia">
-
+      
+	  <br> 14.
    <p>13. Quin port utilitza el TFTP?</p>
-      <input type="radio" name="pregunta13" value="a">a. 69<br>
-      <input type="radio" name="pregunta13" value="b">b. 22<br>
-      <input type="radio" name="pregunta13" value="c">c. 23 <br>
-      <input type="radio" name="pregunta13" value="d">d. 777<br>
-      <input type="submit" value="Envia">
-
+      <input type="radio" name="pregunta14" value="a">a. 69<br>
+      <input type="radio" name="pregunta14" value="b">b. 22<br>
+      <input type="radio" name="pregunta14" value="c">c. 23 <br>
+      <input type="radio" name="pregunta14" value="d">d. 777<br>
+      
+	  <br> 15.
  <p>13. Que es NTP?</p>
-      <input type="radio" name="pregunta13" value="a">a. Nice TP<br>
-      <input type="radio" name="pregunta13" value="b">b. Network time protocol<br>
-      <input type="radio" name="pregunta13" value="c">c. Next time post <br>
-      <input type="radio" name="pregunta13" value="d">d. No te preocupes<br>
+      <input type="radio" name="pregunta15" value="a">a. Nice TP<br>
+      <input type="radio" name="pregunta15" value="b">b. Network time protocol<br>
+      <input type="radio" name="pregunta15" value="c">c. Next time post <br>
+      <input type="radio" name="pregunta15" value="d">d. No te preocupes<br>
+      
+	  <br> 16.
+ <p>13. Que son les CUPS?</p>
+      <input type="radio" name="pregunta16" value="a">a. sistema de impresión de código abierto y modular<br>
+      <input type="radio" name="pregunta16" value="b">b. Son capsules de nespresso<br>
+      <input type="radio" name="pregunta16" value="c">c. Corporation Unificated Post Self<br>
+      <input type="radio" name="pregunta16" value="d">d. Cap de les 3 respostes<br>
       <input type="submit" value="Envia">
 
- <p>13. Que son les CUPS?</p>
-      <input type="radio" name="pregunta13" value="a">a. sistema de impresión de código abierto y modular<br>
-      <input type="radio" name="pregunta13" value="b">b. Son capsules de nespresso<br>
-      <input type="radio" name="pregunta13" value="c">c. Corporation Unificated Post Self<br>
-      <input type="radio" name="pregunta13" value="d">d. Cap de les 3 respostes<br>
-      <input type="submit" value="Envia">
-  
     </form>
   <?php
   } else {
@@ -247,7 +245,7 @@
     if ($pregunta7 == "d") {
       $puntuacio++;
     }
-   
+
  if ($pregunta8 == "a") {
       $puntuacio++;
     }
@@ -281,13 +279,13 @@
 
     // afegiu més preguntes segons les necessitats
 $total_preguntas = 16;
-
+$errors =  $total_preguntas - $puntuacio;
 // Calcular la nota
 $nota = ($puntuacio /$total_preguntas) * 10;
     echo "Nom de l'estudiant: $nom_estudiant<br>";
     echo "Nota: " . number_format($nota, 2);
-    echo "Encertades: " $puntuacio;
-    echo "Incorrectes: " $total_preguntas - $puntuacio;
+    echo "<br>Encertades: $puntuacio<br> ";
+    echo "Incorrectes: $errors<br> ";
   }
   ?>
 </body>
